440c2823701d35102c1206c8545536f24f517e2e
Elena Jerdeva <lena.jerdeva28@gmail.com>
2023-01-12 11:07:52 +0200

    Create nums

--- homework2part/Program.cs
+++ homework2part/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Eneter N");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[n];
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(1, 26);
+    Console.WriteLine(nums[i]);
+}
 
